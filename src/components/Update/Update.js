500ddd12e2676fea70977d0565b8fb17bec8d929
Ben Miller <benbmiller123@gmail.com>
2019-11-17 23:31:29 -0500

    added update method, not working

--- src/components/Update/Update.js
+++ src/components/Update/Update.js
@@ -2,8 +2,21 @@ import React, { Component } from "react";
 import "./Update.css";
 
 class Update extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
   handleSubmit(evt) {
     //handles the update call to the api. make the text input's value a state so you can retrieve it here or something
+
+    let body = evt.target.parentNode.childNodes[1].value;
+    fetch(this.props.url + "/country/" + this.props.match.params.country, {
+      method: "PUT",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   }
 
   render() {
